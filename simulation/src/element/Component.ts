525589c5a532086c08fa7b73f512ab4d500025c8
Shehab <6315412@bdeb.qc.ca>
2026-02-20 14:48:03 -0500

    structure d'algo

---
+++ simulation/src/element/Component.ts
@@ -0,0 +1,14 @@
+export abstract class Component {
+ id : string;
+ node1 : string 
+ node2 : string
+
+ constructor(id: string, node1: string, node2: string){
+    this.id= id;
+    this.node1 = node1;
+    this.node2 = node2;
+ }
+stamp(matrix: number[][], nodeMap: Map<string,number>){
+ }
+
+}
