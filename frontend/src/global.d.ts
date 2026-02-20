4f500139770a5c3403772f27bcb8eeefd2132ccf
SlouKeeper <tomhoff2006@gmail.com>
2026-02-20 15:27:34 -0500

    Base pour Home et modification de Index
    Base pour Home et modification de Index

---
+++ frontend/src/global.d.ts
@@ -0,0 +1,11 @@
+declare module '*.css';
+declare module '*.scss';
+declare module '*.sass';
+declare module '*.less';
+
+// Static asset types (optional)
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';
+declare module '*.gif';
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,21 +1,20 @@
-import { useEffect, useState } from 'react'
-import { NavLink } from 'react-router'
-import { createSimulationWorker } from 'simulation';
+import './Home.css';
 
 function Home() {
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    const worker = createSimulationWorker()
-    worker.onmessage = (e) => {
-      console.log('Result:', e.data);
-    }
-    const input = {test : 5}
-    worker.postMessage(input)
-  })
-
   return (
-<>Home</>
+    <div className='home-container'>
+      
+
+      <div className='hero'>
+        <h1 >Home</h1>
+        <p >Welcome to my app!</p>
+      </div>
+
+
+      f
+    </div>
+
+
   )
 }
 
