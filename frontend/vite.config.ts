f5d1cd12168dbc5abdc2b790d30de7d678350fcb
Zakabyle <souflizakaria1@gmail.com>
2026-02-27 15:50:43 -0500

    Refactor UI components: add ButtonGroup and Application layout, update routes and dependencies

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
