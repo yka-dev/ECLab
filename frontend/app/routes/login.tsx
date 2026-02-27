29979b9ec4540178b3bec4cddfaa88ff7f24443c
SlouKeeper <tomhoff2006@gmail.com>
2026-02-27 14:01:38 -0500

    feat: add light logo SVG and welcome component with dark mode support
    - Added `logo-light.svg` for light theme branding.
    - Created `welcome.tsx` component to display the welcome page with light and dark logo support.
    - Updated `components.json` to configure component settings.
    - Added favicon.ico for improved branding.
    - Introduced `react-router.config.ts` for server-side rendering configuration.

---
+++ frontend/app/routes/login.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "~/components/login-form";
+
+export default function Login() {
+    return  <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-4xl">
+        <LoginForm />
+      </div>
+    </div>
+}
