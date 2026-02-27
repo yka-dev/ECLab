3587553c78b25fdaced55003c4d0ffe72fdedf4a
Yassine <yassine_akh@proton.me>
2026-02-27 19:10:07 +0000

    Mise à jour des types de données et des requêtes pour la gestion des projets et des membres de projet

--- backend/db/migrations/000003_create_projects_table.up.sql
+++ backend/db/migrations/000003_create_projects_table.up.sql
@@ -1,16 +1,16 @@
 CREATE TYPE project_role as ENUM ('owner', 'editor', 'viewer');
 
 CREATE TABLE IF NOT EXISTS projects (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS project_members (
-    id SERIAL PRIMARY KEY,
-    project_id INTEGER NOT NULL,
-    user_id INTEGER NOT NULL,
+    id BIGSERIAL PRIMARY KEY,
+    project_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
     role project_role NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE,
