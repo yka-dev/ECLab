1e3e717637c594f9ae3d6df93c9f1add031a2c0e
yka-dev <yassine_akh@proton.me>
2026-04-19 15:24:15 -0400

    env

--- backend/env/env.go
+++ backend/env/env.go
@@ -1,30 +1,35 @@
 package env
 
-
 import (
+	"errors"
 	"log"
 	"os"
-	
+
 	"github.com/joho/godotenv"
 )
 
 type Env struct {
-	DATABASE_URL string
-	PORT string
+	DATABASE_URL  string
+	PORT          string
 	BREVO_API_KEY string
-	URL string
+	URL           string
 }
 
 func InitEnv() (Env, error) {
 	if err := godotenv.Load(); err != nil {
-		log.Printf("Failed to load environnement variables : %s\n", err)
-		return Env{}, err
+		log.Printf("Failed to load local environnement variables : %s\n", err)
 	}
 
-	return Env{
-		DATABASE_URL:         os.Getenv("DATABASE_URL"),
-		PORT:                 os.Getenv("PORT"),
-		BREVO_API_KEY:        os.Getenv("BREVO_API_KEY"),
-		URL:                  os.Getenv("URL"),
-	}, nil
+	env := Env{
+		DATABASE_URL:  os.Getenv("DATABASE_URL"),
+		PORT:          os.Getenv("PORT"),
+		BREVO_API_KEY: os.Getenv("BREVO_API_KEY"),
+		URL:           os.Getenv("URL"),
+	}
+
+	if env.URL == "" {
+		return Env{}, errors.New("failed to load env variables")
+	}
+
+	return env, nil
 }
